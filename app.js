c8ca8f7c2031f267ba8d80bbb11b8016febfc82b
Angel Gonzalez <116753348+gonsar2211@users.noreply.github.com>
2024-01-27 05:20:17 -0500

    solución Terminada

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora del desafio';
+
+function verConsola() {
+    console.log('El botón fue clicado');
+}
+
+function preguntarNombreDeCiudadDeBrasil() {
+    let nombreDeCiudadDeBrasil = prompt('indica una ciudad de Brazil');
+    alert(`Estuve en ${nombreDeCiudadDeBrasil} y me acordé de ti`);
+}
+
+function presionarBotonAlerta() {
+    alert('Yo amo JS');
+}
+
+function sumaDeDosNumeros() {
+    let primerNumero = parseInt(prompt('Digita un número:'));
+    let segundoNumero = parseInt(prompt('Digita otro Numero:'));
+    let resultadoDeSuma = primerNumero + segundoNumero;
+    alert(`El resultado de tu suma es: ${resultadoDeSuma}`);
+}
 
